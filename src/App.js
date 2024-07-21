21d5f5c02268702e00c7a6f29741a07280880bda
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-21 16:19:46 +0700

    Menggunakan Event Handler Di Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 function App() {
+  function handleNext() {
+    alert("Next Button Clicked");
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
         <div className="active">1</div>
         <div className="">2</div>
-        <div className="">2</div>
+        <div className="">3</div>
       </div>
       <p className="message">Step: Dream</p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#526D82", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#526D82", color: "#fff" }}
+          onMouseEnter={() => alert("prev hover")}
+        >
           Prev
         </button>
-        <button style={{ backgroundColor: "#526D82", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#526D82", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
